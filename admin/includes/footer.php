7faa066b1bb12ec6fa8663f00537875ef9c60e1c
Tinh D. Nguyen <70399886+xdtn7@users.noreply.github.com>
2021-10-27 06:27:36 +0700

    Delete admin directory

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>@<a href="#">Mustafa</a></b>
-    </div>
-    <strong>Copyright &copy; 2018 HRM, Attendance and Payroll System </strong>
-</footer>
